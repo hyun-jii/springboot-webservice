64d48b4a5b140828325d3b90da2dbabd0cccda09
hyun-jii <tldnals49@naver.com>
2020-02-09 19:09:03 +0900

    #생성시간/ 수정시간 자동 엔티티

---
+++ src/main/java/com/hyunjii/springboot/domain/posts/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.hyunjii.springboot.domain.posts;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 상속 시 필드들도 컬럼으로 인식
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    // Entity 생성 시 생성시간 자동 저장
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    // Entity 변경 시 변경 시간 자동 저장
+    @LastModifiedBy
+    private LocalDateTime modifiedDate;
+}
