9cb0100f0a5ac453e8ee8e69170b00a5bdde937b
hyun-jii <tldnals49@naver.com>
2020-02-12 20:40:59 +0900

    #컨트롤러 조회 추가

--- src/main/java/com/hyunjii/springboot/web/IndexController.java
+++ src/main/java/com/hyunjii/springboot/web/IndexController.java
@@ -1,19 +1,31 @@
 package com.hyunjii.springboot.web;
 
+import com.hyunjii.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+    // read
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts",postsService.findAllDesc());
+
         return "index";
     }
 
+    // insert
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
+
+
 
 }
