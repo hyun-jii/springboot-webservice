a336c6decf2076d2e5dc3986fe8b2137f1552ab3
hyun-jii <tldnals49@naver.com>
2020-02-12 21:22:28 +0900

    #수정 폼 이동 컨트롤러 추가

--- src/main/java/com/hyunjii/springboot/web/IndexController.java
+++ src/main/java/com/hyunjii/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.hyunjii.springboot.web;
 
 import com.hyunjii.springboot.service.posts.PostsService;
+import com.hyunjii.springboot.web.dto.PostsResponseDto;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,7 +15,7 @@ public class IndexController {
 
     private final PostsService postsService;
 
-    // read
+    //조회, 메인 화면 이동
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts",postsService.findAllDesc());
@@ -20,12 +23,20 @@ public class IndexController {
         return "index";
     }
 
-    // insert
+    //insert 화면 이동
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
 
+    //update 화면 이동
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
 
 
 }
