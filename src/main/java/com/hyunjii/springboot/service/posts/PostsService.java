aff8e63f65c1b1cdad91f982423edcebedf01176
hyun-jii <tldnals49@naver.com>
2020-02-12 20:26:30 +0900

    #컨트롤러 조회 추가

--- src/main/java/com/hyunjii/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyunjii/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hyunjii.springboot.service.posts;
 
 import com.hyunjii.springboot.domain.posts.Posts;
 import com.hyunjii.springboot.domain.posts.PostsRepository;
+import com.hyunjii.springboot.web.dto.PostsListResponseDto;
 import com.hyunjii.springboot.web.dto.PostsResponseDto;
 import com.hyunjii.springboot.web.dto.PostsSaveRequestDto;
 import com.hyunjii.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,6 +42,14 @@ public class PostsService {
                 new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return  new PostsResponseDto(entity);
+    }
+
+    // 조회
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 
