def8dbb97b4866901e9c5527bbddf3f17b564411
hyun-jii <tldnals49@naver.com>
2020-02-15 17:04:41 +0900

    #오타 수정 class -> interface

--- src/main/java/com/hyunjii/springboot/domain/user/UserRepository.java
+++ src/main/java/com/hyunjii/springboot/domain/user/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     // 이미 가입된 사용자인지 확인
     Optional<User> findByEmail(String email);
